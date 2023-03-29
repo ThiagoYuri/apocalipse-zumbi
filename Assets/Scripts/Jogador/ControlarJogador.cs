f6c2e4b8b24ee551a6a6723c335a5ba9951c18ab
ThiagoYuri <thiagoyuri0109@gmail.com>
2023-03-28 23:53:21 -0300

    Update Controller player

--- Assets/Scripts/Jogador/ControlarJogador.cs
+++ Assets/Scripts/Jogador/ControlarJogador.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class ControlarJogador : MonoBehaviour
 {
     public float Velocidade = 10;
+    Vector3 direcao;
+
     // Update is called once per frame
     void Update()
     {
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ =  Input.GetAxis("Vertical");
 
-        Vector3 direcao = new Vector3(eixoX,0,eixoZ);
+        direcao = new Vector3(eixoX,0,eixoZ);
 
-        transform.Translate(direcao* Velocidade * Time.deltaTime);
         if(direcao !=  Vector3.zero){
             GetComponent<Animator>().SetBool("Movendo",true);
         }else{
             GetComponent<Animator>().SetBool("Movendo",false);
         }
+    }
+
+    void FixedUpdate() {        
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao* Velocidade * Time.deltaTime));        
     }
 }
